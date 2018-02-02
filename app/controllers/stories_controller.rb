0cbd025156002a52724a4482f1441d3d43099fb1
Pat Johnston <pattrashgarbage@gmail.com>
2018-02-02 04:47:31 -0500

    Style full page & responsive

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -7,7 +7,24 @@ class StoriesController < ApplicationController
     # Pull story data and store in storyArray
     i = 0
     while i < 3 do
-      @storyArray.push(HTTParty.get("https://hacker-news.firebaseio.com/v0/item/#{@storyIDs[i]}.json"))
+      currentStory = HTTParty.get("https://hacker-news.firebaseio.com/v0/item/#{@storyIDs[i]}.json")
+
+      # Calculate hours since story was posted
+      secondsSincePost = ((Time.now.to_i - currentStory["time"]))
+      hoursSincePost = secondsSincePost / 3600
+      currentStory["timeStamp"] = hoursSincePost.floor
+
+      puts currentStory
+      # Shorten URL to main source website
+      if currentStory["url"]
+        urlTemp = currentStory["url"].split("/")
+        urlTemp[2].gsub! "www.", ""
+        currentStory["urlShort"] = urlTemp[2]
+      else 
+        currentStory["urlShort"] = "source"
+      end
+
+      @storyArray.push(currentStory)
       i += 1
     end
       # by = story.by
