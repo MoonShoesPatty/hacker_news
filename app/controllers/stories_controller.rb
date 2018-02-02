8fddf308f5b335a043f7be5a4954be9e081160e0
Pat Johnston <pattrashgarbage@gmail.com>
2018-02-02 02:38:21 -0500

    style page content

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -6,7 +6,7 @@ class StoriesController < ApplicationController
     @storyArray = []
     # Pull story data and store in storyArray
     i = 0
-    while i < 3 do
+    while i < 30 do
       @storyArray.push(HTTParty.get("https://hacker-news.firebaseio.com/v0/item/#{@storyIDs[i]}.json"))
       i += 1
     end
